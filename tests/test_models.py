fd6aed5e165e574fba991c33a2a75ee531eca49d
emilio <emilio93erv@gmail.com>
2025-11-24 18:27:04 +0100

    tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,8 +1,34 @@
-from src.models import RegistroPeso
+from src.models import Usuario, RegistroPeso
+from src.db import get_connection, init_db, DB_PATH
+import os
 
-def test_model_to_dict():
-    reg = RegistroPeso(70, "2025-11-20")
-    d = reg.to_dict()
+def setup_function():
+    if DB_PATH.exists():
+        os.remove(DB_PATH)
+    init_db()
 
-    assert d["peso"] == 70
-    assert d["fecha"] == "2025-11-20"
+def test_insert_user():
+    user_id = Usuario.registrar("a@a.com", "1234", 180)
+    assert user_id == 1
+
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT email, altura FROM users WHERE id=1")
+    row = cur.fetchone()
+    conn.close()
+
+    assert row[0] == "a@a.com"
+    assert row[1] == 180
+
+def test_insert_registro():
+    user_id = Usuario.registrar("a@a.com", "1234", 180)
+    RegistroPeso.crear(user_id, 70, "2025-11-20")
+
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT peso, fecha FROM registros WHERE user_id=?", (user_id,))
+    row = cur.fetchone()
+    conn.close()
+
+    assert row[0] == 70
+    assert row[1] == "2025-11-20"
