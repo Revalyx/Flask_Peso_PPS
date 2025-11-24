c29844b1f602525bbb9e3ae0651958ab0c257225
emilio <emilio93erv@gmail.com>
2025-11-24 18:25:57 +0100

    tests

--- src/db.py
+++ src/db.py
@@ -1,36 +1,32 @@
-# db.py
+from pathlib import Path
 import sqlite3
-import os
 
-
-DB_PATH = os.path.join(os.path.dirname(__file__), "..", "peso.db")
-
+DB_PATH = Path("peso.db")
 
 def get_connection():
     return sqlite3.connect(DB_PATH)
-
 
 def init_db():
     conn = get_connection()
     cur = conn.cursor()
 
     cur.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL,
-        altura REAL NOT NULL
-    )
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            email TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            altura REAL
+        )
     """)
 
     cur.execute("""
-    CREATE TABLE IF NOT EXISTS registros (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id INTEGER NOT NULL,
-        peso REAL NOT NULL,
-        fecha TEXT NOT NULL,
-        FOREIGN KEY(user_id) REFERENCES users(id)
-    )
+        CREATE TABLE IF NOT EXISTS registros (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            peso REAL,
+            fecha TEXT,
+            FOREIGN KEY(user_id) REFERENCES users(id)
+        )
     """)
 
     conn.commit()
